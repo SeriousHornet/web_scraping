7e67b71a198eeab840548fb083f556b933876119
Manu <64640197+SeriousHornet@users.noreply.github.com>
2022-11-27 13:57:49 +0800

    initial commit

---
+++ FBRef.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://fbref.com/en/comps/Big5/stats/players/Big-5-European-Leagues-Stats"
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, "html.parser")
+results = soup.find(id="ResultsContainer")
+print(results.prettify())
