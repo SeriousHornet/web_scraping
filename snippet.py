ce25c40a7140df8c64d77b3add418aee3bb584da
Manoj Seenivasan <serioushornet@proton.me>
2022-12-05 18:25:19 +0800

    able to get data from fotmob by finding 'script' tags

--- snippet.py
+++ snippet.py
@@ -1,31 +1,65 @@
+import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+import json
+
+headers = {'User-Agent':
+               'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 '
+               'Safari/537.36'}
 
 # specify the URL
-url = "https://www.fotmob.com/match/3370551/matchfacts/argentina-vs-australia"
+base_url = "https://www.fotmob.com/match/"
+# match = str(input('Please enter the match ID: '))
+match = str(3370551)
+url = base_url + match
 
 # send a request to the URL and get the HTML response
-response = requests.get(url)
+pageTree = requests.get(url, headers=headers)
 
 # parse the HTML response using BeautifulSoup
-soup = BeautifulSoup(response.text, 'html.parser')
+pageSoup = BeautifulSoup(pageTree.content, 'lxml')
 
-# find all the circle tags with the ID "circle" in the HTML response
-tags = soup.find_all('circle', id='circle')
-
-# print the scraped data
-for circle in tags:
-    print(circle)
+# find all the script tags
+scripts = pageSoup.find_all('script')
 
 # check if any matching tags were found
-if len(tags) == 0:
+if len(scripts) == 0:
     print("No matching tags were found.")
 else:
-    # print the scraped data
-    for tag in tags:
-        print(tag)
+    print("Tags found.")
 
-# print the cx and cy attributes of each circle tag
-for circle in tags:
-    print(circle.get('cx'))
-    print(circle.get('cy'))
+# get the last string
+strings = scripts[-1].string
+# print(strings)
+
+# delete the last bit, to get just the useful information
+ind_end = strings.index('"page":')
+json_data = strings[:ind_end]
+repl = "}"
+json_data = json_data[:-1] + repl
+#
+
+raw_data = json.loads(json_data)
+# print(type(raw_data))
+
+
+df_general = raw_data["props"]["pageProps"]["general"]
+# print(len(df_general.items()))
+# print(df_general.items())
+df_content = raw_data["props"]["pageProps"]["content"]
+df_shots = df_content["shotmap"]["shots"]
+print(type(df_shots))
+shots_dict = {}
+for item in df_shots:
+    for i in item:
+        shots_dict.update(item)
+    shots_dict.update(item)
+
+print(shots_dict)
+# shot_df = pd.DataFrame.from_dict(shots_dict)
+# print(len(shots_dict.keys()))
+# print(shot_df)
+# print(len(df_shots.items()))
+# print(shots.items())
+# df_shots = df_content["shotmap"]["shots"]
+# print(df_shots)
