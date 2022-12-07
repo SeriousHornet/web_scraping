5f07bc503b823dcb97a48bb19b14adbf250054c3
Manoj Seenivasan <serioushornet@proton.me>
2022-12-07 15:30:21 +0800

    Updated the working folder

---
+++ snippets_fotmob.py
@@ -0,0 +1,106 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+import pandas as pd
+import matplotlib.pyplot as plt
+from mplsoccer import Pitch
+
+pd.set_option('display.max_columns', 50)
+headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 '
+                  'Safari/537.36'}
+
+# specify the URL
+base_url = "https://www.fotmob.com/match/"
+# match = str(input('Please enter the match ID: '))
+match = str(3370557)
+url = base_url + match
+
+# send a request to the URL and get the HTML response
+pageTree = requests.get(url, headers=headers)
+
+# parse the HTML response using BeautifulSoup
+pageSoup = BeautifulSoup(pageTree.content, 'lxml')
+
+# find all the script tags
+scripts = pageSoup.find_all('script')
+
+# check if any matching tags were found
+if len(scripts) == 0:
+    print("No matching tags were found.")
+else:
+    print("Tags found.")
+
+# get the last string
+strings = scripts[-1].string
+
+# delete the last bit, to get just the useful information
+ind_end = strings.index('"page":')
+json_data = strings[:ind_end]
+repl = "}"
+json_data = json_data[:-1] + repl
+
+raw_data = json.loads(json_data)
+
+# extracting needed data from raw data
+general = raw_data["props"]["pageProps"]["general"]
+roundName = general['leagueRoundName']
+homeTeam = general['homeTeam']['name']
+homeTeamID = general['homeTeam']['id']
+awayTeam = general['awayTeam']['name']
+awayTeamID = general['awayTeam']['id']
+content = raw_data["props"]["pageProps"]["content"]
+shots = content["shotmap"]["shots"]
+
+df_shots = pd.DataFrame(shots)
+float_conv = {'x': float, 'y': float, 'min': int, 'blockedX': float, 'blockedY': float, 'expectedGoals': float,
+              'expectedGoalsOnTarget': float, 'goalCrossedY': float, 'goalCrossedZ': float}
+# print(content)
+df_shots = df_shots.astype(float_conv)
+df_shots['conv_x'] = df_shots['x'] * 1.142857142857143
+df_shots['conv_y'] = df_shots['y'] * 1.176470588235294
+df_shots['xG'] = df_shots['expectedGoals'] * 200
+
+home_df = df_shots[df_shots.teamId == homeTeamID]
+away_df = df_shots[df_shots.teamId == awayTeamID]
+
+
+def call_fig():
+    fig, axs = plt.subplots(figsize=(6, 8))
+    fig.set_facecolor('#3e3e40')
+    axs.patch.set_facecolor('#3e3e40')
+
+    pitch = Pitch(half=True,
+                  pitch_color='#3e3e40',
+                  line_color='#aba9a8')
+
+    pitch.draw(ax=axs)
+    plt.gca().invert_yaxis()
+
+
+def plotting(df):
+    for i, row in df.iterrows():
+        X, Y, xG = df.loc[i, 'conv_x'], df.loc[i, 'conv_y'], df.loc[i, 'xG']
+        if df.loc[i, 'eventType'] == "Goal":
+            plt.scatter(X, Y, c='lime', marker='*', s=xG)
+        if df.loc[i, 'eventType'] == "AttemptSaved":
+            plt.scatter(X, Y, c='darkorange', s=xG)
+        if df.loc[i, 'eventType'] == "Miss":
+            plt.scatter(X, Y, c='r', s=xG)
+    return
+
+
+call_fig()
+team = input("Select home or away team: ")
+if team == 'home':
+    plotting(home_df)
+    plt.title(f"{homeTeam} shots \n vs. {awayTeam} in {roundName}", fontsize=20, color="white")
+    plt.savefig(f'Shots of {homeTeam} vs. {awayTeam} in {roundName}.png',
+                transparent=False,
+                dpi=1000)
+if team == 'away':
+    plotting(away_df)
+    plt.title(f'{awayTeam} shots \n vs. {homeTeam}', fontsize=20, color="white")
+    plt.savefig(f'Shots of {awayTeam} vs. {homeTeam} in {roundName}.png',
+                transparent=False,
+                dpi=1000)
---
+++ understat_scrap.py
@@ -0,0 +1,84 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+import pandas as pd
+import matplotlib.pyplot as plt
+from mplsoccer import Pitch
+import seaborn as sns
+
+pd.set_option('display.max_columns', 50)
+base_url = 'https://understat.com/match/'
+match = str(input('Please enter the match id: '))
+url = base_url + match
+# url = 'https://understat.com/match/19773'
+
+res = requests.get(url)
+soup = BeautifulSoup(res.content, 'lxml')
+
+scripts = soup.findAll('script')
+
+strings = scripts[1].string
+ind_start = strings.index("('") + 2
+ind_end = strings.index("')")
+
+json_data = strings[ind_start:ind_end]
+json_data = json_data.encode('utf8').decode('unicode_escape')
+raw_data = json.loads(json_data)
+
+data_home = raw_data['h']
+data_away = raw_data['a']
+home = pd.DataFrame.from_dict(data_home)
+away = pd.DataFrame.from_dict(data_away)
+
+float_conv = {'X': float, 'Y': float, 'xG': float, 'minute': int, 'h_goals': int, 'a_goals': int}
+
+away = away.astype(float_conv)
+home = home.astype(float_conv)
+
+away['x'] = away['X'] * 120
+away['y'] = away['Y'] * 80
+away['xg'] = away['xG'] * 250
+
+home['x'] = home['X'] * 120
+home['y'] = home['Y'] * 80
+home['xg'] = home['xG'] * 250
+
+fig, axs = plt.subplots(figsize=(6, 8))
+fig.set_facecolor('#3e3e40')
+axs.patch.set_facecolor('#3e3e40')
+
+pitch = Pitch(half=True,
+              pitch_color='#3e3e40',
+              line_color='#aba9a8')
+
+pitch.draw(ax=axs)
+plt.gca().invert_yaxis()
+
+
+def plotting(df):
+    for x in range(len(df['X'])):
+        X, Y, xG = df['x'][x], df['y'][x], df['xg'][x]
+        if df['result'][x] == "Goal":
+            plt.scatter(X, Y, c='lime', s=xG)
+        if df['result'][x] == "SavedShot":
+            plt.scatter(X, Y, c='r', s=xG)
+        if df['result'][x] == "BlockedShot":
+            plt.scatter(X, Y, c='darkorange', s=xG)
+    return
+
+
+# h_team = home.loc['h_team'][0]
+# a_team = away.loc['a_team'][0]
+
+team = input("Select home or away team: ")
+if team == 'home':
+    plotting(home)
+    # plt.title(f'{h_team} Shots \n taken +vs. +{a_team} ')
+
+if team == 'away':
+    plotting(away)
+    # plt.title(f'{a_team} Shots \n taken +vs. +{h_team} ')
+
+print(home.columns)
+
+plt.show()
